36982b5c1fda2ee376d5b70d1fc53f33a5cee933
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-09-25 09:27:58 -0500

    Trace CEF process entry in diagnostics mode

--- src/cef_process.cpp
+++ src/cef_process.cpp
@@ -1,5 +1,9 @@
 #include "goreecloud/browser/cef_process.hpp"
 
+#include <cstdlib>
+#include <iostream>
+#include <string_view>
+
 #if GOREECLOUD_ENABLE_CEF
 #include "goreecloud/browser/cef_media_probe_app.hpp"
 #include "include/cef_app.h"
@@ -9,9 +13,22 @@ namespace goreecloud::browser {
 
 int execute_cef_subprocess_if_needed(int argc, char** argv) {
 #if GOREECLOUD_ENABLE_CEF
+  const char* diagnostics =
+      std::getenv("GOREECLOUD_BROWSER_RUNTIME_DIAGNOSTICS");
+  const bool diagnostic =
+      diagnostics && *diagnostics && std::string_view{diagnostics} != "0";
+  if (diagnostic) {
+    std::cerr << "[GoreeCloud CEF] entering CefExecuteProcess" << std::endl;
+  }
+
   CefMainArgs main_args(argc, argv);
   CefRefPtr<GoreeCloudCefRenderApp> app = new GoreeCloudCefRenderApp();
   const int exit_code = CefExecuteProcess(main_args, app, nullptr);
+
+  if (diagnostic) {
+    std::cerr << "[GoreeCloud CEF] CefExecuteProcess returned "
+              << exit_code << std::endl;
+  }
   return exit_code;
 #else
   (void)argc;
