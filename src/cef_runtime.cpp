f433ecd462db215e52d24c5eba28932ea81f474b
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-09-25 15:44:34 -0500

    Use the browser-only CEF app in CefInitialize

--- src/cef_runtime.cpp
+++ src/cef_runtime.cpp
@@ -244,7 +244,7 @@ class CefRuntimeDelegateScaffold final : public ChromiumRuntimeDelegate {
     CefString(&settings.root_cache_path) = options_.cache_root.string();
     CefString(&settings.locale) = options_.locale;
 
-    CefRefPtr<GoreeCloudCefRenderApp> app = new GoreeCloudCefRenderApp();
+    CefRefPtr<GoreeCloudCefBrowserApp> app = new GoreeCloudCefBrowserApp();
     if (cef_runtime_diagnostics_enabled()) {
       std::cerr << "[GoreeCloud CEF] entering CefInitialize" << std::endl;
     }
