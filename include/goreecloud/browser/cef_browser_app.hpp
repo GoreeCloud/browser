b4cdebeb2908a9ccbde6d2489c6f0644b6d55e60
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-09-26 16:52:13 -0500

    Reuse existing CEF browser app through shared helper

--- include/goreecloud/browser/cef_browser_app.hpp
+++ include/goreecloud/browser/cef_browser_app.hpp
@@ -1,38 +1,10 @@
 #pragma once
 
-#include <atomic>
-
-#if GOREECLOUD_ENABLE_CEF
-#include "include/cef_app.h"
-#endif
+#include "goreecloud/browser/cef_media_probe_app.hpp"
 
 namespace goreecloud::browser {
 
 #if GOREECLOUD_ENABLE_CEF
-
-class GoreeCloudCefBrowserApp final : public CefApp,
-                                      public CefBrowserProcessHandler {
- public:
-  GoreeCloudCefBrowserApp() = default;
-
-  CefRefPtr<CefBrowserProcessHandler> GetBrowserProcessHandler() override {
-    return this;
-  }
-
-  void OnContextInitialized() override {
-    browser_context_initialized_.store(true, std::memory_order_release);
-  }
-
-  [[nodiscard]] bool browser_context_initialized() const noexcept {
-    return browser_context_initialized_.load(std::memory_order_acquire);
-  }
-
- private:
-  std::atomic_bool browser_context_initialized_{false};
-
-  IMPLEMENT_REFCOUNTING(GoreeCloudCefBrowserApp);
-  DISALLOW_COPY_AND_ASSIGN(GoreeCloudCefBrowserApp);
-};
 
 inline CefRefPtr<GoreeCloudCefBrowserApp> goreecloud_cef_browser_app() {
   static CefRefPtr<GoreeCloudCefBrowserApp> app = new GoreeCloudCefBrowserApp();
