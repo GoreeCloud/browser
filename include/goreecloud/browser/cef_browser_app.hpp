28b870333255bb9658c22b7425b18def6c1ebd45
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-09-26 17:12:05 -0500

    Skip Chromium first-run UI in embedded Browser runtime

--- include/goreecloud/browser/cef_browser_app.hpp
+++ include/goreecloud/browser/cef_browser_app.hpp
@@ -1,13 +1,32 @@
 #pragma once
 
-#include "goreecloud/browser/cef_media_probe_app.hpp"
+#include "include/cef_app.h"
+#include "include/cef_command_line.h"
 
 namespace goreecloud::browser {
 
 #if GOREECLOUD_ENABLE_CEF
 
-inline CefRefPtr<GoreeCloudCefBrowserApp> goreecloud_cef_browser_app() {
-  static CefRefPtr<GoreeCloudCefBrowserApp> app = new GoreeCloudCefBrowserApp();
+class GoreeCloudCefProcessApp final : public CefApp {
+ public:
+  GoreeCloudCefProcessApp() = default;
+
+  void OnBeforeCommandLineProcessing(
+      const CefString& process_type,
+      CefRefPtr<CefCommandLine> command_line) override {
+    if (process_type.empty() && command_line &&
+        !command_line->HasSwitch("no-first-run")) {
+      command_line->AppendSwitch("no-first-run");
+    }
+  }
+
+ private:
+  IMPLEMENT_REFCOUNTING(GoreeCloudCefProcessApp);
+  DISALLOW_COPY_AND_ASSIGN(GoreeCloudCefProcessApp);
+};
+
+inline CefRefPtr<CefApp> goreecloud_cef_browser_app() {
+  static CefRefPtr<CefApp> app = new GoreeCloudCefProcessApp();
   return app;
 }
 
